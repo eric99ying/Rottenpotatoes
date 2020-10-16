63f9968cfc2eb71556fde9ef219073c088a44783
Eric Ying <eying@berkeley.edu>
2020-10-16 06:02:34 +0000

    Got part 1 working

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,10 @@
 class Movie < ActiveRecord::Base
+  
+  def self.all_ratings
+    return Movie.distinct.pluck(:rating)
+  end
+  
+  def self.with_ratings(ratings)
+    return Movie.where(rating:ratings)
+  end
 end
